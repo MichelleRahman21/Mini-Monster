539f63c518b7a557654140135bb87bce5acce965
michelleRahman21 <michgomez1807@hotmail.com>
2020-05-20 12:29:47 -0400

    Added filter monsters as a constant
    to filter the monsters
    
    to be later use on the CardList route

--- src/App.js
+++ src/App.js
@@ -20,7 +20,9 @@ componentDidMount(){
 }
 
 render() {
-  const { monsters, searchField } = this.state; 
+  const { monsters, searchField } = this.state;
+  const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
+)
   return (
     <div className="App">
     <input
@@ -28,7 +30,7 @@ render() {
     placeholder='search monsters'
     onChange={e =>
       this.setState({searchField: e.target.value})}/>
-      <CardList monsters={this.state.monsters}>
+      <CardList monsters={filteredMonsters}>
       </CardList>
     </div>
   );
