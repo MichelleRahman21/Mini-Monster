27dd9538c0e813d23fd2a08bd0d5a256a542879d
michelleRahman21 <michgomez1807@hotmail.com>
2020-05-19 21:46:15 -0400

    Added searchField to setState
    Targeted value from users onChange
    
    Create an input from users so they can use the search field

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super();
 
  this.state = {
-  monsters: []
+  monsters: [],
+  searchField: ''
   };
 }
 
@@ -21,6 +22,11 @@ componentDidMount(){
 render() {
   return (
     <div className="App">
+    <input
+    type='search'
+    placeholder='search monsters'
+    onChange={e =>
+      this.setState({searchField: e.target.value})}/>
       <CardList monsters={this.state.monsters}>
       </CardList>
     </div>
