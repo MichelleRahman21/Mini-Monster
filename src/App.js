86847501fb6f7af09f24a0a48b92f3f9ff80b753
michelleRahman21 <michgomez1807@hotmail.com>
2020-05-21 13:26:57 -0400

    Added SearchBox functional component
    Clean up code by exporting search box and make
    
    it a reusable component.
    
    Use props to use in our app.js to have
    
    same functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { CardList } from './components/card-list/card-list';
+import { SearchBox } from './components/search-box/search-box';
 import './App.css';
 
 
@@ -25,11 +26,11 @@ render() {
 )
   return (
     <div className="App">
-    <input
-    type='search'
-    placeholder='search monsters'
-    onChange={e =>
-      this.setState({searchField: e.target.value})}/>
+      <SearchBox
+      placeholder='search monsters'
+      handleChange={e =>
+        this.setState({searchField: e.target.value})}
+      />
       <CardList monsters={filteredMonsters}>
       </CardList>
     </div>
