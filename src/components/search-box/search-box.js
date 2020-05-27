9e55f43b59ab315675618c5c2d448adc4d53386b
michelleRahman21 <michgomez1807@hotmail.com>
2020-05-27 12:25:39 -0400

    Completed project

--- src/components/search-box/search-box.js
+++ src/components/search-box/search-box.js
@@ -4,10 +4,10 @@ import React from 'react';
 import './search-box';
 
 export const SearchBox = props => (
-<input
-  className='search-box'
-  type='search'
-  placeholder='search monsters'
-  onChange={props.onSearchChange}
-/>
+    <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={props.onSearchChange}
+    />
 );
