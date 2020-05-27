d6faaf3d142a799bb87fb74d3f9065973ba19e29
michelleRahman21 <michgomez1807@hotmail.com>
2020-05-27 12:53:16 -0400

    Remove logo png from manifest.json
    Add different fetch call with
    
    mode: no-cors

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 }
 
 componentDidMount(){
-  fetch('https://jsonplaceholder.typicode.com/users')
+  fetch('https://jsonplaceholder.typicode.com/users', {mode: 'no-cors'})
   .then(response => response.json())
   .then(users => this.setState({ monsters: users}));
 }
