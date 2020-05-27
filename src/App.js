a505aec0b08b53b362bf3abbb11029810166d102
michelleRahman21 <michgomez1807@hotmail.com>
2020-05-27 12:55:35 -0400

    Bug fix

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 }
 
 componentDidMount(){
-  fetch('https://jsonplaceholder.typicode.com/users', {mode: 'no-cors'})
+  fetch('https://jsonplaceholder.typicode.com/users')
   .then(response => response.json())
   .then(users => this.setState({ monsters: users}));
 }
